cb4f2b3f4e5e6a579d2696f91bbdf44aa6170925
KJAnusha <44720787+KJAnusha@users.noreply.github.com>
2020-08-17 20:32:24 +0530

    Update login.js

--- Node_Assign7/features/step_definitions/login.js
+++ Node_Assign7/features/step_definitions/login.js
@@ -11,22 +11,21 @@ defineSupportCode(function ({Given,When,Then}) {
 
     Given(/^I Navigate to opencart website$/,{timeout:1000},function (callback){
                 this.driver.get("https://www.opencart.com");
-        // let Title = this.driver.getTitle();
-        //         console.log(Title);
                 callback();
     });
+	
     Given('Verify the title as {string}',{timeout:1000},function (string, callback){
-
                    this.driver.getTitle()
                  .then(function(title) {
                      assert.deepStrictEqual(string, title);
                  });
             callback();
     });
+	
     When('Click on login link',{timeoutInMilliseconds:2000}, function(callback){
-
         this.driver.findElement({xpath:".//a[@class='btn btn-link navbar-btn']"}).click().then(callback);
     });
+	
     When(/^User logs in using wrong "([^"]*)" or "([^"]*)"$/,{timeoutInMilliseconds:2000}, function (username,password,callback) {
             this.driver.findElement({xpath:".//input[@name='email']"}).sendKeys(username).then(callback);
             this.driver.findElement({xpath:".//input[@name='password']"}).sendKeys(password).then(callback);
@@ -34,12 +33,12 @@ defineSupportCode(function ({Given,When,Then}) {
     });
 
    Then('Verify the login error message as {string}',{timeoutInMilliseconds: 30000}, function (string) {
-
         this.driver.findElement({xpath:".// div[@class='alert alert-danger']"})
            .getText().then(function (result) {
                 console.log("Error Message is: "+result);
             return assert.assert.equal(result,string);
        });
+	   
    When('click on close',{timeout:1000}, function(callback){
 	this.driver.findElement({xpath:".//input[@name='close']"}).click().then(callback);
 	});
